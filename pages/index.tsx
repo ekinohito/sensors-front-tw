7322b0ecfdccbc363e62974e635ed9e87bb05ba6
ekinohito <zhenyagur9@gmail.com>
2021-10-09 15:50:13 +0300

    basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
-  return (
-      <div>
-
-      </div>
-  )
+    return (
+        <>
+            <header className="bg-blue-500">
+                <div className="container mx-auto sticky py-3">
+                    Header
+                </div>
+            </header>
+            <div className="container mx-auto grid grid-cols-3 space-x-7 my-6">
+                <div className="col-span-2 bg-gray-500 text-center">Graph</div>
+                <div className="bg-red-500">Buttons</div>
+            </div>
+        </>
+    )
 }
 
 export default Home
