708c094b9e5e0e661d81bfb7d87d7cbef3bac0a3
frake23 <dimsacom@gmail.com>
2021-10-09 18:50:30 +0300

    default vals

--- src/components/PointsForm.tsx
+++ src/components/PointsForm.tsx
@@ -17,12 +17,12 @@ export const PointsForm: React.FC<PointsFormProps> = ({onSubmit, onCancel}) => {
                 className="flex flex-col"
                 onSubmit={handleSubmit(onSubmit)}
             >
-                <DataInput name="min_x" label="Минимум x" control={control}/>
-                <DataInput name="max_x" label="Максимум x" control={control}/>
-                <DataInput name="min_y" label="Минимум y" control={control}/>
-                <DataInput name="max_y" label="Максимум x" control={control}/>
-                <DataInput name="quantity" label="Количество точек" control={control}/>
-                <DataInput name="sleep" label="Интервал передачи" control={control}/>
+                <DataInput name="min_x" label="Минимум x" control={control} defaultValue={0}/>
+                <DataInput name="max_x" label="Максимум x" control={control} defaultValue={10}/>
+                <DataInput name="min_y" label="Минимум y" control={control} defaultValue={1}/>
+                <DataInput name="max_y" label="Максимум x" control={control} defaultValue={6}/>
+                <DataInput name="quantity" label="Количество точек" control={control} defaultValue={11}/>
+                <DataInput name="sleep" label="Интервал передачи" control={control} defaultValue={0.5}/>
                 <div className="flex flex-row justify-around flex-wrap">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white
